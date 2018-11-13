a857e576c5f80b18fa84fa072c43897a396b481f
Sotiris Liagas <liagason@gmail.com>
2018-11-13 23:34:52 +0200

    Update pnft.py

--- pnft.py
+++ pnft.py
@@ -1,6 +1,9 @@
 import time
-
-x = 500000
+print('Prime Number Finder & Timer [Version: 1.0.0.1, Compiled: Tuesday, 13 Nov 2018]')
+print('Copyright (c) 2018, Sotiris Liagas. All rights reserved.\n')
+print('Primality test algorithm is based on trial division and is NOT optimized for speed.')
+print('Source code & documentation are available at https://github.com/liagason\n\')
+x = int(input('Find all prime numbers up to (e.g.: 500000):'))
 
 t0 = time.time()
 for num in range(2,x):
