30822165b39b33924fb7a6858854472c932f912d
liagason <liagason@gmail.com>
2018-11-14 18:40:08 +0200

    Converted code to C#

---
+++ Prime Number Finder Timer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Management;
+using System.Diagnostics;
+namespace Prime_rangeber_Finder_Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Number Finder & Timer [Version: 1.0.0.2, Compiled: Tuesday, 13 Nov 2018]");
+            Console.WriteLine("Copyright (c) 2018, Sotiris Liagas. All rights reserved.\n");
+            Console.WriteLine("Primality test algorithm is based on trial division and is NOT optimized for speed.");
+            Console.WriteLine("Source code & documentation are available at https://github.com/liagason\n");
+            Console.WriteLine("Find all prime numbers up to (e.g. 500000):");
+            int m;
+            string input = Console.ReadLine();
+            long range;
+            Int64.TryParse(input, out range);
+            bool isPrimeNumber = true;
+            if (range >= 2 && range is long)
+            {
+                for (m = 0; m < 2; m++)
+                {
+                    Stopwatch stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    for (long i = 2; i <= range; i++)
+                    {
+                        for (long j = 2; j <= range; j++)
+                        {
+
+                            if (i != j && i % j == 0)
+                            {
+                                isPrimeNumber = false;
+                                break;
+                            }
+                        }
+                        if (isPrimeNumber && m == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        isPrimeNumber = true;
+                    }
+                    stopWatch.Stop();
+                    TimeSpan ts = stopWatch.Elapsed;
+                    if (m == 0) Console.WriteLine("[Needed " + ts + " to FIND AND PRINT all prime numbers up to " + range + "]");
+                    if (m == 1) Console.WriteLine("[Needed " + ts + " to JUST PRINT all prime numbers up to " + range + "]");
+                }
+                ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+                foreach (ManagementObject obj in myProcessorObject.Get())
+                {
+                    Console.WriteLine("[Tested on " + obj["Name"] + "]");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fatal error. Input must be an integer number.");
+            }
+            Console.WriteLine("Press ENTER to exit...");
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Prime-Number-Finder-Timer
-A trial division primality test algorithm implemented in Python and Autoit aimed to check CPU speed.
+A trial division primality test algorithm implemented in C# aimed to check CPU speed.
 
 ## INFO
 1. A simple tool developed to be used by non-technical users aiming to check & compare CPU speed.
 
 2. Basic algorithm functionality is implemented in Python.
-The compiled version was developed with Autoit v3 as it produces very small standalone executables for Windows compared to Python compilers.
+The compiled version was developed with C# as it produces very small standalone executables for Windows compared to other compilers. Tested with .NET Framework v4.6.1 but should work with earlier versions.
 
 ## DOWNLOAD
 1. [Download v.1.0.0.1 for Windows x86](https://github.com/liagason/Prime-Number-Finder-Timer/blob/master/pnft.exe "Prime Number Finder Timer x86")
